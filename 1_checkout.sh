d7ff7cbde2f364d0cc04b26aff383f01eb6774fe
Dominik Rose <dominik.rose@leanix.net>
2018-07-17 16:12:44 +0200

    First steps towards easier setup

--- 1_checkout.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-projects="service-cart service-dataloader service-navigation service-product service-recommendation service-user frontend-catalog frontend-checkout frontend-registration"
-
-for project in $projects; do
-  git clone git@github.com:dominikrose-leanix/appstash-$project.git
-done
