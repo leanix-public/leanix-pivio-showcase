515ce9a0f4924eea1b0105f045b22dab4c4022a6
Julian Ludwig <julian.ludwig@leanix.net>
2019-01-30 15:14:31 +0100

    adaption of script 1_create_github_repos.sh

--- 1_create_github_repos.sh
+++ 1_create_github_repos.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+read -p "Please enter your github username:" username
+read -s -p "Please enter your github password:" pswd
+
+
+
+
 projects="service-cart service-dataloader service-navigation service-product service-recommendation service-user frontend-catalog frontend-checkout frontend-registration"
 
+
 for project in $projects; do
-  curl -u $1 https://api.github.com/user/repos -d '{"name":"appstash-'$project'"}'  
+  curl -u "$username:$pswd" https://api.github.com/user/repos -d '{"name":"appstash-'$project'"}'  
 
 done
